09e6dd09efcdf86f381dab806c55e3483bf7f090
Feifei Jiang <Feifei.Jiang@outlook.com>
2018-04-27 09:46:52 +0800

    Add create new employee action.

--- WebApplication/Controllers/EmployeeController.cs
+++ WebApplication/Controllers/EmployeeController.cs
@@ -60,5 +60,10 @@
             }
             return View(viewListModel);
         }
+
+        public ActionResult AddNew()
+        {
+            return View("CreateEmployye");
+        }
     }
 }
