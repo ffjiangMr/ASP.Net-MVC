05221c426baef402a09b305f51e9c218b117f38e
Feifei Jiang <Feifei.Jiang@outlook.com>
2018-05-02 10:06:05 +0800

    Add customer modelbinder.

---
+++ WebApplication/Controllers/Binder/SensitiveWorldFilterModelBinder.cs
@@ -0,0 +1,33 @@
+﻿namespace WebApplication.Controllers.Binder
+{
+    #region using directive
+
+    using System;
+    using System.ComponentModel;
+    using System.Web.Mvc;
+
+    #endregion
+
+    public class SensitiveWorldFilterModelBinder : DefaultModelBinder
+    {
+        protected override void SetProperty(ControllerContext controllerContext, ModelBindingContext bindingContext, PropertyDescriptor propertyDescriptor, object value)
+        {
+            if (propertyDescriptor.PropertyType == typeof(String))
+            {
+                value = bindingContext.PropertyFilter(value as String);
+            }
+            base.SetProperty(controllerContext, bindingContext, propertyDescriptor, value);
+        }
+
+        public override object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var value = base.BindModel(controllerContext, bindingContext);
+            if (bindingContext.ModelType == typeof(String))
+            {
+                value = bindingContext.PropertyFilter(value as String);
+            }
+            return value;
+        }
+
+    }
+}
--- WebApplication/Models/Employee.cs
+++ WebApplication/Models/Employee.cs
@@ -11,7 +11,9 @@
     {
         [Key]
         public Int32 EmployeeId { get; set; }
+        [Required(ErrorMessage ="Enter First Name")]
         public String FirstName { get; set; }
+        [StringLength(5,ErrorMessage ="Last Name length should not be greater than 5")]
         public String LastName { get; set; }
         public Int32 Salary { get; set; }
     }
