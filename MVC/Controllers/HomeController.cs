34831b9d7aeabafa459c5fde40e104aea347a3e4
feifei.jiang@outlook.com <Feifei.Jiang@outlook.com>
2018-09-17 16:27:20 +0800

    update

---
+++ MVC/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using MVC.Models;
+using System;
+using System.Web.Mvc;
+using System.Web;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        // GET: Home        
+        public ActionResult Index()
+        {
+            Int32 time = DateTime.Now.Hour;
+            ViewBag.Greeting = time < 12 ? "Good morning" : "Good afternoon";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ MVC/Models/GuestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public String Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(".+\\@.+\\..+",ErrorMessage ="Please enter a valid email address")]
+        public String Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public String Phone { get; set; }
+
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
+        public Boolean? WillAttend { get; set; }
+    }
+}
