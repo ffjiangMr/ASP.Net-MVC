8326a7fd6b46e94d0f3e7c35c60443c792ff8705
feifei.jiang@outlook.com <Feifei.Jiang@outlook.com>
2018-09-18 15:49:00 +0800

    update

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LanguageFeatures.Models;
+using System.Web.Mvc;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public String Index()
+        {
+            return "Navigate to a URL to show an example";
+        }
+
+        public ViewResult AutoProperty()
+        {
+            Product myProduct = new Product();
+            myProduct.Name = "Kayak";
+            String productName = myProduct.Name;
+            return View("Result", (Object)$"Product name: {productName}");
+        }
+    }
+}
---
+++ LanguageFeatures/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        private String name;
+
+        public String Name { get => name; set => name = value; }
+    }
+}
