bfee12da4926bbd6b74d6525d7181d8deb2c8c0f
feifei.jiang@outlook.com <Feifei.Jiang@outlook.com>
2018-09-18 16:44:02 +0800

    update

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -22,5 +22,27 @@ namespace LanguageFeatures.Controllers
             String productName = myProduct.Name;
             return View("Result", (Object)$"Product name: {productName}");
         }
+
+        public ViewResult CreateProduct()
+        {
+            Product myProduct = new Product();
+            myProduct.Name = "Kayak";
+            myProduct.Price = 275M;
+            myProduct.ProductID = 100;
+            myProduct.Description = "A boat for one person";
+            myProduct.Category = "Watersports";
+            return View("Result", (Object)$"Category:{myProduct.Category}");
+        }
+
+        public ViewResult CreateCollection()
+        {
+            String[] stringArray = { "apple", "orange", "plum" };
+            List<Int32> intList = new List<Int32> { 10, 20, 30, 40 };
+            Dictionary<String, Int32> myDict = new Dictionary<String, Int32>
+            {
+                { "apple",10},{ "orange",20},{ "plum",30}
+            };
+            return View("Result", (Object)stringArray[1]);
+        }
     }
 }
--- LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/Models/Product.cs
@@ -8,7 +8,10 @@ namespace LanguageFeatures.Models
     public class Product
     {
         private String name;
-
-        public String Name { get => name; set => name = value; }
+        public String Name { get { return this.ProductID.ToString() + this.name; } set { this.name = value; } }
+        public Int32 ProductID { get; set; }
+        public String Description { get; set; }
+        public Decimal Price { get; set; }
+        public String Category { get; set; }
     }
 }
