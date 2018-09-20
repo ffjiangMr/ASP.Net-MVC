614a95cb29077b3cf8ef2e356fb44452f64b691d
feifei.jiang@outlook.com <Feifei.Jiang@outlook.com>
2018-09-20 15:49:24 +0800

    update

---
+++ EssentialTools.Test/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTools.Models;
+using EssentialTools.Infrastructure;
+
+namespace EssentialTools.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        private IDiscountHelper GetTestObject()
+        {
+            return new MinimumDiscountHelper();
+        }
+
+        [TestMethod]
+        [Owner("ffjiang")]
+        public void Discount_Above_100()
+        {
+            IDiscountHelper target = this.GetTestObject();
+            Decimal total = 200;
+
+            var discountTotal = target.ApplyDiscount(total);
+
+            Assert.AreEqual(total * 0.9M, discountTotal);
+        }
+    }
+}
