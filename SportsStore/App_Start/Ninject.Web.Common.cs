ee630a72b1e5a4d2a376cc68131d00ba7c86c4d4
feifei.jiang@outlook.com <Feifei.Jiang@outlook.com>
2018-09-21 14:23:00 +0800

    update

--- SportsStore/App_Start/Ninject.Web.Common.cs
+++ SportsStore/App_Start/Ninject.Web.Common.cs
@@ -11,21 +11,22 @@ namespace SportsStore.App_Start
     using Ninject;
     using Ninject.Web.Common;
     using Ninject.Web.Common.WebHost;
+    using SportsStore.Infrastructure;
 
-    public static class NinjectWebCommon 
+    public static class NinjectWebCommon
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
 
         /// <summary>
         /// Starts the application
         /// </summary>
-        public static void Start() 
+        public static void Start()
         {
             DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
             DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
             bootstrapper.Initialize(CreateKernel);
         }
-        
+
         /// <summary>
         /// Stops the application.
         /// </summary>
@@ -33,7 +34,7 @@ namespace SportsStore.App_Start
         {
             bootstrapper.ShutDown();
         }
-        
+
         /// <summary>
         /// Creates the kernel that will manage your application.
         /// </summary>
@@ -61,6 +62,7 @@ namespace SportsStore.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
-        }        
+            System.Web.Mvc.DependencyResolver.SetResolver(new NinjectDependencyResolver(kernel));
+        }
     }
 }
---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using SportsStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductsRepository repository;
+
+        public ProductController(IProductsRepository productRepository)
+        {
+            this.repository = productRepository;
+        }
+
+        [HttpGet]
+        public ViewResult List()
+        {
+            return View(this.repository.Products);
+
+        }
+    }
+}
