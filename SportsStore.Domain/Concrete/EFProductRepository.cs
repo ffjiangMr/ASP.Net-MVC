85a1c43e64e7c5a35600687946fbd269d2513d72
feifei.jiang@outlook.com <Feifei.Jiang@outlook.com>
2018-09-29 15:17:12 +0800

    update

--- SportsStore.Domain/Concrete/EFProductRepository.cs
+++ SportsStore.Domain/Concrete/EFProductRepository.cs
@@ -1,6 +1,8 @@
 ﻿using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
 using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace SportsStore.Domain.Concrete
 {
@@ -11,6 +13,26 @@ namespace SportsStore.Domain.Concrete
         public IEnumerable<Product> Products
         {
             get { return this.context.Products; }
+        }
+
+        public void SaveProduct(Product prop)
+        {
+            if (prop.ProductID == 0)
+            {
+                this.context.Products.Add(prop);
+            }
+            else
+            {
+                Product entity = this.context.Products.Find(prop.ProductID);
+                if (entity != null)
+                {
+                    entity.Name = prop.Name;
+                    entity.Category = prop.Description;
+                    entity.Price = prop.Price;
+                }
+            }
+
+            this.context.SaveChanges();
         }
     }
 }
