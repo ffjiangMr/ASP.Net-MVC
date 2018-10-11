d6c9aa8fdef14279b366dd7b3ce47dd9f60695e8
feifei.jiang@outlook.com <Feifei.Jiang@outlook.com>
2018-10-11 16:19:05 +0800

    update

---
+++ ControllerAndActions/Controllers/DerivedController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllerAndActions.Controllers
+{
+    public class DerivedController : Controller
+    {
+        public ViewResult Index()
+        {
+            this.ViewBag.Message = "Hello from the DerivedController Index method";
+            return View("MyView");
+        }
+    }
+}
