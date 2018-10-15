3294784c2587458eb6ccdc63146816c2988ee8ff
feifei.jiang@outlook.com <Feifei.Jiang@outlook.com>
2018-10-15 10:34:02 +0800

    update

---
+++ Filters/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Filters.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(String userName, String password, String returnUrl)
+        {
+            Boolean result = FormsAuthentication.Authenticate(userName, password);
+            if (result)
+            {
+                FormsAuthentication.SetAuthCookie(userName, false);
+                return Redirect(returnUrl ?? Url.Action("Index", "Admin"));
+            }
+            else
+            {
+                ModelState.AddModelError("","Incorrect userName or password");
+                return View();
+            }
+        }
+
+    }
+}
--- Filters/Controllers/HomeController.cs
+++ Filters/Controllers/HomeController.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Filters.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
 
