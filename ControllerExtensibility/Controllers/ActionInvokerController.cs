1012a068543b7455ba0dc11e0da0228a2626bc0f
feifei.jiang@outlook.com <Feifei.Jiang@outlook.com>
2018-10-17 14:41:17 +0800

    update

---
+++ ControllerExtensibility/Controllers/ActionInvokerController.cs
@@ -0,0 +1,16 @@
+﻿using ControllerExtensibility.Infrastructure;
+
+using System.Web.Mvc;
+
+namespace ControllerExtensibility.Controllers
+{
+    public class ActionInvokerController : Controller
+    {
+
+        public ActionInvokerController()
+        {
+            this.ActionInvoker = new CustomActionInvoker();
+        }
+
+    }
+}
