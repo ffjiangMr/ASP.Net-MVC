d03e575218be95a10e5c47f2e82a96d4b294d8d5
feifei.jiang@outlook.com <Feifei.Jiang@outlook.com>
2018-10-17 09:24:55 +0800

    update

---
+++ ControllerExtensibility/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using ControllerExtensibility.Models;
+
+using System.Web.Mvc;
+
+namespace ControllerExtensibility.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ViewResult Index()
+        {
+            return View("Result", new Result()
+            {
+                ControllerName = "Product",
+                ActionName = "Index",
+            });
+        }
+
+        public ViewResult List()
+        {
+            return View("Result", new Result()
+            {
+                ControllerName = "Product",
+                ActionName = "List",
+            });
+        }
+    }
+}
---
+++ ControllerExtensibility/Models/Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControllerExtensibility.Models
+{
+    public class Result
+    {
+        public String ControllerName { get; set; }
+
+        public String ActionName { get; set; }
+    }
+}
