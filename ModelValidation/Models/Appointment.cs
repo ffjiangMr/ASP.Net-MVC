f71a0f83c03c7b0fddeef5a7ee2cf004b04d768c
Feifei Jiang <Feifei.Jiang@outlook.com>
2018-11-12 16:34:30 +0800

    update

---
+++ ModelValidation/Models/Appointment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ModelValidation.Models
+{
+    public class Appointment
+    {
+        public String ClientName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        public Boolean TermsAccepted { get; set; }
+    }
+}
