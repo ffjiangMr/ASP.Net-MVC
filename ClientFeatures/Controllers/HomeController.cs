41575cdfd8b3402e2fd955078356f6acb2e84268
Feifei Jiang <Feifei.Jiang@outlook.com>
2018-11-13 16:59:35 +0800

    update

---
+++ ClientFeatures/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using ClientFeatures.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ViewResult MakeBooking()
+        {
+            return View(new Appointment()
+            {
+                ClientName = "Adam",
+                TermAccepted = true,
+            });
+        }
+
+        [HttpPost]
+        public JsonResult MakeBooking(Appointment appt)
+        {
+            return Json(appt, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ ClientFeatures/Models/Appointment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClientFeatures.Models
+{
+    public class Appointment
+    {
+        [Required]
+        public String ClientName { get; set; }
+
+        public Boolean TermAccepted { get; set; }
+    }
+}
